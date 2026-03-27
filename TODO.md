1d47a6c4342d39b038bb560f76250099e7acfded
Jerome Etienne <jerome.etienne@gmail.com>
2026-03-27 01:55:29 +0100

    adding a example to handle streaming

---
+++ TODO.md
@@ -0,0 +1,3 @@
+- bug when streaming responses with caching enabled
+  - https://claude.ai/chat/04993974-9834-4d98-90ac-943492ef2bc0
+  - claude.ai is telling me it is possible if SSE chunks is handled
---
+++ examples/openai_streaming_basic.ts
@@ -0,0 +1,49 @@
+import Path from 'path';
+
+import { Cacheable } from 'cacheable';
+import OpenAICache from 'openai-cache';
+import KeyvSqlite from '@keyv/sqlite';
+import { OpenAI } from 'openai';
+
+// const PROJECT_ROOT = Path.resolve(__dirname, '../');
+
+async function main() {
+
+	///////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////
+	//	
+	///////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////
+
+	// initialize OpenAI client with caching
+	const sqlitePath = `sqlite://${Path.resolve(__dirname, `./.openai_cache.sqlite`)}`;
+	const sqliteCache = new Cacheable({ secondary: new KeyvSqlite(sqlitePath) });
+	const openaiCache = new OpenAICache(sqliteCache);
+	const openaiClient = new OpenAI({
+		fetch: openaiCache.getFetchFn()
+	});
+
+	///////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////
+	//	
+	///////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////
+
+	// create a streaming response
+	const stream = await openaiClient.responses.create({
+		model: 'gpt-4.1-nano',
+		input: 'Say "Sheep sleep deep" ten times fast!',
+		stream: true,
+	});
+
+	// consume the stream
+	for await (const _event of stream) {
+		if (_event.type !== 'response.output_text.delta') continue
+		const event = _event as OpenAI.Responses.ResponseTextDeltaEvent;
+
+		process.stdout.write(event.delta);
+	}
+}
+
+
+main().catch(console.error);
